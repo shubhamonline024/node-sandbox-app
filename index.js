381c18b041222f70b26e74b6f70fe134586e3fec
Shubham Online <shubhamonline024@gmail.com>
2025-11-30 11:51:52 +0530

    json middleware

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
-const app = express();
 const PORT = 80;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const output = (req, res) => {
   return {
